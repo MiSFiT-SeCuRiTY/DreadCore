6c320a526448d023e574b2a3c64564b1765c3d32
MiSFiT <106611003+MiSFiT-SeCuRiTY@users.noreply.github.com>
2026-09-15 07:36:07 +0530

    Delete web directory

--- web/web/static/js/dread.js
+++
@@ -1,2 +0,0 @@
-function toggleSide(){document.getElementById('side')?.classList.toggle('open')}
-function toast(msg){let x=document.createElement('div');x.className='toast';x.textContent=msg;document.body.appendChild(x);setTimeout(()=>x.remove(),2600)}
